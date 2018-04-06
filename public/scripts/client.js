4cb7000d085276993a6b2c65014ed0925f4288f6
Bryan Rieger <bryan@yiibu.com>
2018-04-05 18:57:11 -0700

    Ditch text scaling for the demo.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,85 +6,15 @@
 
 // https://gist.github.com/mekwall/1263939
 
-(function($) {
-  $.fn.textFit = function(defaultSize) {
-    return this.each(function() {
-      var card = $(this).parent()[0],
-          panel = this,
-          text = $("p", this)[0],
-          maxFontSize = parseInt($(panel).attr("data-font-size") || defaultSize),
-          currentFontSize = parseInt(window.getComputedStyle(panel).getPropertyValue("font-size")),
-          padding = parseInt(window.getComputedStyle(text).padding),
-          fontSize = currentFontSize;
-      while (text.clientHeight <= panel.clientHeight && fontSize < maxFontSize) {
-        fontSize = fontSize + 2;
-        panel.style.fontSize = fontSize + "px";
-      }
-      while (text.scrollHeight - padding > panel.clientHeight - padding) {
-        fontSize = fontSize - 2;
-        panel.style.fontSize = fontSize + "px";
-      }
-    });
-  };
-})(jQuery);
-
 $(function() {
-  var throttle = function(type, name, obj) {
-    obj = obj || window;
-    var running = false;
-    var func = function() {
-      if (running) {
-        return;
-      }
-      running = true;
-      requestAnimationFrame(function() {
-        obj.dispatchEvent(new CustomEvent(name));
-        running = false;
-      });
-    };
-    obj.addEventListener(type, func);
-  };
-  throttle("resize", "optimizedResize");
 
-  function updateText(event) {
-    $(".panel.text").textFit("15px");
-  }
-
-  function updateArrows(event, slick) {
-    if (slick.currentSlide <= 0) {
-      $(".slick-prev").addClass("dim");
-    } else {
-      $(".slick-prev").removeClass("dim");
-    }
-    if (slick.currentSlide >= slick.slideCount - 1) {
-      $(".slick-next").addClass("dim");
-    } else {
-      $(".slick-next").removeClass("dim");
-    }
-  }
-
-  var playingVideo, playingiFrame;
-  var textPanels = $(".panel.text");
-  textPanels.each(function (i, obj) {
-    if (obj.style.fontSize) {
-      $(obj).attr('data-font-size', obj.style.fontSize);
-    }
-  });
-
-  window.addEventListener("optimizedResize", function() {
-    updateText();
-  });
+  var playingVideo;
 
   $(".stack").on("beforeChange", function(event, slick, currentSlide, nextSlide) {
 
     var id = $(slick.$slides.get(nextSlide)).attr('id'),
     iframe = $("#" + id).find("iframe");
-
-    if (playingiFrame) {
-      playingiFrame.each(function (i,obj) {
-          // $(obj).attr("src", null);
-      })
-    }
+    window.location.hash = id;
 
     if (iframe.length) {
       iframe.each(function (i, obj) {
@@ -92,15 +22,12 @@ $(function() {
           $(obj).attr("src", $(obj).attr("data-src"));
         }
       });
-      playingiFrame = iframe;
     }
 
   });
 
   $(".stack").on("afterChange", function(event, slick, currentSlide) {
-    updateArrows(event, slick);
-    window.location.hash = id;
-    
+
     var id = $(slick.$slides.get(currentSlide)).attr('id'),
         video = $("#" + id).find("video");
 
@@ -119,17 +46,13 @@ $(function() {
   });
 
   $(".stack").on("init", function(event, slick) {
-    updateArrows(event, slick);
-    updateText();
+    
   });
 
   $(".stack").slick({
-    dots: true,
-    arrows: true,
+    arrows: false,
     infinite: false,
-    waitForAnimate: false,
-    appendDots: $("main"),
-    appendArrows: $("main")
+    waitForAnimate: false
   });
 
   if(window.location.hash) {
@@ -138,4 +61,5 @@ $(function() {
     var card = $('.stack .card').index(id);
     $(".stack").slick('slickGoTo', card, true);
   }
+
 });
