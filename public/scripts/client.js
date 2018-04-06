271e289081287adb5a6e7bfd57bec524c03a4875
Bryan Rieger <bryan@yiibu.com>
2018-04-06 11:47:58 -0700

    Fixed text resizing! :)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,77 @@
-// client-side js
-// run by the browser each time your view template is loaded
 
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
+// https://developer.mozilla.org/en-US/docs/Web/Events/resize
+var optimizedResize = (function() {
+  var callbacks = [],
+      running = false;
+  function resize() {
+      if (!running) {
+          running = true;
+          if (window.requestAnimationFrame) {
+              window.requestAnimationFrame(runCallbacks);
+          } else {
+              setTimeout(runCallbacks, 66);
+          }
+      }
+  }
+  function runCallbacks() {
+      callbacks.forEach(function(callback) {
+          callback();
+      });
+      running = false;
+  }
+  function addCallback(callback) {
+      if (callback) {
+          callbacks.push(callback);
+      }
+  }
+  return {
+      add: function(callback) {
+          if (!callbacks.length) {
+              window.addEventListener('resize', resize);
+          }
+          addCallback(callback);
+      }
+  }
+}());
 
-// https://gist.github.com/mekwall/1263939
+
+// adapted from https://gist.github.com/andrewbranch/6995056
+(function($) {
+  function AutoShrinker($element) {
+      this.$element = $element;
+      this.$text = $("p", $element);
+      this.initialFontSize = parseFloat($element.css("fontSize"));
+      this.currentFontSize = this.initialFontSize;
+      this.padding = parseFloat(this.$text.css("padding"));
+      this.delta = 2;
+      this.resize = function() {
+        var maxHeight = this.$element.height() - this.padding,
+            newFontSize = this.currentFontSize;
+
+        while ((this.$text[0].scrollHeight - this.padding - this.delta) <= this.$element[0].clientHeight && (this.currentFontSize + this.delta) < this.initialFontSize) {
+          newFontSize = newFontSize + this.delta;
+          this.$element.css("fontSize", newFontSize);
+        }
+        while ((this.$text[0].scrollHeight - this.padding - this.delta) > maxHeight) {
+          newFontSize = newFontSize - this.delta;
+          this.$element.css("fontSize", newFontSize);
+        }
+        this.currentFontSize = newFontSize;
+      };
+  }
+  $.fn.autoshrink = function() {
+      return this.each(function() {
+          var shrinker, $this = $(this);
+          $this.data("autoshrinker", shrinker = new AutoShrinker($this));
+          shrinker.resize();
+          optimizedResize.add(function() {
+            shrinker.resize();
+        });
+      });
+  };
+})(jQuery);
+
+// end of plugins
 
 $(function() {
 
@@ -62,4 +129,5 @@ $(function() {
     $(".stack").slick('slickGoTo', card, true);
   }
 
+  $(".panel.text").autoshrink();
 });
